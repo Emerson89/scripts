cc2a1abb8f17f975a3fb56e4b74267ab79267309
Emerson <you@example.com>
2021-10-02 09:53:19 -0300

    add file

---
+++ rds_disk_alocado.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+import boto3
+import sys
+
+instanceid = sys.argv[1]
+nregion = sys.argv[2]
+accessKey = sys.argv[3]
+secretKey = sys.argv[4]
+def disk():
+    client = boto3.client('rds', aws_access_key_id=accessKey, aws_secret_access_key=secretKey, region_name=nregion)
+    responsedx = client.describe_db_instances()
+    for v in responsedx['DBInstances']:
+     if v['DBInstanceIdentifier'] == instanceid:
+        filtredInstance = v
+    alocado = filtredInstance['AllocatedStorage']
+    print(alocado)
+disk()
